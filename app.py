90b2a767f7c45bc20a1ee55870e32fe91a9f8019
Ana Hocevar <ahocevara@gmail.com>
2018-03-21 17:11:05 +0000

    hide quandl key

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from bokeh.plotting import figure, show, save
 from bokeh.io import output_file
 from bokeh.models import ColumnDataSource
 from bokeh.embed import components
+import os
 
 app = Flask(__name__)
 
@@ -28,9 +29,11 @@ def index():
         today = now.strftime("%Y-%m-%d")
         then = now - timedelta(days=31)
         first_day = then.strftime("%Y-%m-%d")
+        
+        my_Quandl_key = os.environ.get('QuandlKey', None)
 
         #Get Data from www.quandl.com
-        r = requests.get('https://www.quandl.com/api/v3/datasets/WIKI/{0}.json?column_index=4&start_date={1}&end_date={2}&api_key=zRAuGY8-NoKwgZWbTa_a'.format(this_ticker,first_day,today))
+        r = requests.get('https://www.quandl.com/api/v3/datasets/WIKI/{0}.json?column_index=4&start_date={1}&end_date={2}&api_key=my_Quandl_key'.format(this_ticker,first_day,today))
         
         if r.status_code == 403:
             return ("You do not have permission to view this stock ticker. Please subscribe to this ticker to get access.")
@@ -46,8 +49,8 @@ def index():
         #Bokeh
         source = ColumnDataSource(data=df)
         p = figure(title= this_ticker + " closing price in the last month", x_axis_label='Date', y_axis_label='Closing Price', x_axis_type="datetime", plot_width=600, plot_height=400)
-        p.line(x = 'Date', y = 'Close', source = source, legend = this_ticker, line_width = 2)
-        p.circle(x = 'Date', y = 'Close', source = source, legend = this_ticker)
+        p.line(x = 'Date', y = 'Close', source = source, legend = this_ticker, line_width = 2, color="maroon")
+        p.circle(x = 'Date', y = 'Close', source = source, legend = this_ticker, color="maroon",size=5)
 
 #        output_file("templates/graph.html")
 #        save(p)
