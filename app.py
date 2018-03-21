1306a2183702cfabf7d9f9a26d838bd6a95c44d2
Ana Hocevar <ahocevara@gmail.com>
2018-03-21 22:11:15 +0000

    improved templates

--- app.py
+++ app.py
@@ -15,15 +15,14 @@ app.vars={}
 @app.route('/')
 def start():
     return redirect('/index')
-    
-    
+
 @app.route('/index',methods=['GET','POST'])
 def index():
     if request.method == 'GET':
-        return render_template('index.html')
+        return render_template('myindex.html', title="Home")
     else:
         #request was a POST
-        this_ticker = request.form['this_ticker']
+        this_ticker = request.form['this_ticker'].upper()
         
         now = datetime.today()
         today = now.strftime("%Y-%m-%d")
@@ -50,24 +49,25 @@ def index():
         source = ColumnDataSource(data=df)
         p = figure(title= this_ticker + " closing price in the last month", x_axis_label='Date', y_axis_label='Closing Price', x_axis_type="datetime", plot_width=600, plot_height=400)
         p.line(x = 'Date', y = 'Close', source = source, legend = this_ticker, line_width = 2, color="maroon")
-        p.circle(x = 'Date', y = 'Close', source = source, legend = this_ticker, color="maroon",size=5)
+        p.circle(x = 'Date', y = 'Close', source = source, legend = this_ticker, color="maroon",size=8, alpha=0.5)
 
 #        output_file("templates/graph.html")
 #        save(p)
                 
         script, div = components(p)
 
-        return render_template("graph.html", the_div=div, the_script=script)        
+        return render_template("mygraph.html", title="Stock Ticker Plot", the_div=div, the_script=script)        
 #        return redirect(url_for('graph', the_div=div, the_script=script))
 
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('myabout.html', title="About")
 
-@app.route('/graph')
-def graph():
-    return render_template('graph.html', the_script=request.args.get('the_script'), the_div=request.args.get('the_div'))
+
+#@app.route('/graph')
+#def graph():
+#    return render_template('graph.html', the_script=request.args.get('the_script'), the_div=request.args.get('the_div'))
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
